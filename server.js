2c90e2c98655ca8f85e70aeee1cf3875c56bd0eb
Nguyen Huy Duc <nmaddp199599@gmail.com>
2017-04-03 14:44:02 +0000

    initial commit

--- server.js
+++ server.js
@@ -1,10 +1,73 @@
-var express = require('express')
-var app = express()
 
-app.get('/home', function (req, res) {
-  res.send('Hello World!')
-})
+var express = require("express");
+var bodyParser = require("body-parser");
+var cors = require("cors");
 
-app.listen(8080, function () {
-  console.log('Example app listening on port 8080!')
-})
+var app = express();
+app.use(cors());
+app.use(bodyParser.json());
+app.get('/',function(req,res){
+  res.send("Pass the unix timestamp or a natural language date (example: January 1, 2016) to get the detail of date");
+});
+
+var monthName = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+
+
+function checkUnix(value){
+ var unixTime = Number(value);
+ if((Number.isInteger(unixTime)==true)&&(unixTime>=0)) {
+   return true;
+ } return false;
+ 
+}
+
+
+app.get('/:value',function(req,res){
+  var value = req.params.value;
+  if(checkUnix(value)){
+    var time ;
+    var naturalDate = new Date(Number(value)*1000);
+    
+    var month = naturalDate.getMonth();
+    var year = naturalDate.getFullYear();
+    var day = naturalDate.getDate();
+    naturalDate = monthName[month]+" "+day+", "+year;
+    time = {
+        "unix": Number(value),
+        "natural": naturalDate
+      }
+    res.send(time);
+  } else {
+    try {
+      value = value.split(/(?:,| )+/);
+      var year= value[2];
+      var day = value[1];
+      var month = monthName.indexOf(value[0]);
+      var date = new Date(year,month,day);
+      var unixTime = date.getTime()/1000;
+      var naturalDate = monthName[month]+" "+day+", "+year;
+      var time = {
+        "unix": unixTime,
+        "natural" : naturalDate
+      };
+     
+       if(date=="Invalid Date") res.send("Wrong parameters");
+       else res.send(time);
+    } catch(err){
+      res.send("Wrong parameters");
+    }
+    
+  }
+  
+  
+});
+
+app.listen(8080,function(){
+  console.log("Server is running");
+});
+
+
+
+
+
+
